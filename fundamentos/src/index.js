2fd84622f1093d1f47989fba15ae3ce799ea03c7
Matheus Vasconcelos <matheus.vasco.roch@gmail.com>
2022-08-23 00:18:24 -0300

    Componentes com Propriedades

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom"
 import React from "react"
 
-import Primeiro from "./components/Primeiro"
+import Primeiro from "./components/basicos/Primeiro"
+import ComParametro from "./components/basicos/ComParametro"
 
 import "./index.css"
 
@@ -10,6 +11,8 @@ const el = document.getElementById("root")
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro titulo="Segundo Componente" subtitulo="Muito legal!" numero={9.3}></ComParametro>
+        <ComParametro titulo="Segundo Componente" subtitulo="Mais ou menos!" numero={"6,0"}></ComParametro>
     </div>,
     el
 )
